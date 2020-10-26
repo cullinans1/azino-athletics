5ab2ef5cf090ef2bb516420fc266f73677fcb237
Sammy <sammy.cullinan@gmail.com>
2020-10-26 11:06:08 -0700

    models completed

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Product = require('./Product');
+const Order = require('./Order');
+const Category = require('./Category');
+
+module.exports = { User, Product, Order, Category };
